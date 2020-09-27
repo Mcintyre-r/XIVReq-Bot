b26176cca32f05b73740b4a9007bc08f0c165ff7
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-09-26 20:08:26 -0400

    notes

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ bot.on('message',async req => {
             
         }
     })
-
+    // list of clip names
     const clips = ['women', 'scissors','eekum bokum','really gay','law','gay','center', 'news', 'army', 'leader', 'yeet', 'lid', 'console', 'joker', 'rainbow', 'reyn', 'head', 'good thing', 'tough', 'jump', 'ooph', 'oof', 'vsauce', 'mario']
     clips.forEach( async e =>{
         if(message.includes(e) && req.author.id != 738254569238167643){
