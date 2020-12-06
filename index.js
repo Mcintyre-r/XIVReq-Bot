d323fab2bee16e87630976d75698cbe2945c811a
Ronald McIntyre <ronxcamacho@gmail.com>
2020-12-06 08:47:33 -0500

    setup for heroku

---
+++ index.js
@@ -0,0 +1,77 @@
+const {Client, MessageAttachment, MessageEmbed, Guild} = require('discord.js')
+const bot  = new Client();
+const axios = require('axios')
+require('dotenv').config()
+
+const PREFIX = '?'
+
+bot.on('ready', () =>{
+    console.log('Req-Bot Online')
+})
+ 
+bot.on( 'message' , async message => {
+    let item = message.content.toLowerCase()
+    let post = {
+        item: '',
+        quantity: 0,
+        requestedBy: ''
+    }
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+    const filter = m => m.author.id === message.author.id;
+
+    
+    switch(args[0].toLowerCase()){
+
+        // submits item request for request project
+        case 'request' :
+
+            message.reply("Please submit a quantity... Will expire in 10 seconds..").then(r => r.delete ({timeout: 10000})).catch(err => console.log(err))
+            message.channel.awaitMessages(filter, { max: 1, time: 10000}).then(collected => {
+                const quantity = collected.first().content
+                collected.first().delete({timeout: 1000 * 10})
+
+            // User Checks if submitted request exists and then formats request for submission.     
+            if(item !== '?request') {
+                // parses given number from string to int
+                const trueQuan = parseInt(quantity)      
+                const itemSubmit = item.replace("?request", "")
+                post.quantity = trueQuan;
+                post.requestedBy = message.author.username;
+                post.requesterId = message.author.id;
+                post.requesterPicture = message.author.avatar;
+                post.item = itemSubmit.trimStart()
+                const user = message.author
+                console.log(post)
+                axios.get(`https://xivapi.com/search?string=${post.item}&private_key=73bc4666b8044a95acbe3b469b59c0079beaf9666d164a35a68846fbd4f99f2f`)
+                .then(response => {
+                    const apiItem = response.data.Results
+                    if(apiItem[0]){
+                        console.log(apiItem[0])
+                    post.item = apiItem[0].Name
+                    post.itemIcon = `https://xivapi.com${apiItem[0].Icon}`
+                    post.itemID = apiItem[0].ID
+
+                        console.log(post)
+                    axios.post('https://xivreq.herokuapp.com/api/requests/submit', {post, user} )
+                    .then(res => message.channel.send('Request submitted: \n Check status at http://xivreq.com').then( r => r.delete ({timeout: 15000})).catch(err => console.log(err)))
+                    .catch(err => message.channel.send('There was an error submitting your request. \n Please check the request and try again.').then( r => r.delete ({timeout: 15000})).catch(err => console.log(err)))
+                    } else {
+                        message.channel.send('Cannot find item, check name submission').then( r => r.delete ({timeout: 15000})).catch(err => console.log(err))
+                    }
+                })
+                .catch(err => {
+                    message.channel.send('Cannot find item, check name submission').then( r => r.delete ({timeout: 15000})).catch(err => console.log(err))
+                })
+
+               
+            } else {
+                message.channel.send('Requests cannot be empty.').then( r => r.delete ({timeout: 15000})).catch(err => console.log(err))
+            }
+            })
+            break;
+        
+    }
+})
+ 
+bot.login('NzA2NjY5MTM1OTE1OTA5MTQw.Xq9m6w.cP4gDDcwXBdBNoG6UQfogKDNP4w')
